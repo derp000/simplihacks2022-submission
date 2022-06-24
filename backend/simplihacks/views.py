45bce90fb0fe6b13168f8680ffbdd121494d7024
derp000 <84947701+derp000@users.noreply.github.com>
2022-06-24 13:36:39 -0400

    api yay

--- backend/simplihacks/views.py
+++ backend/simplihacks/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from dotenv import load_dotenv
+import os
+import requests
 
 # Create your views here.
 def index(request):
+    load_dotenv()
+    api = os.getenv("API_KEY")
+    url = f"http://api.weatherapi.com/v1/current.json?key={api}&q=London&aqi=no"
+    
+    print(requests.get(url).json())
+
     return render(request, "simplihacks/index.html")
